28cfb021840a0e4211a389dd42bd29250c3bd53c
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2019-04-17 14:53:07 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 import { debugLog } from './components/utilities';
 
 import {Login} from './components/Login/Login'
-import Register from './components/Register'
+import Registration from './components/Registration'
+import Notfound from './components/notfound'
 import {UpdateProperties} from './components/UpdateProperties/UpdateProperties.jsx';
 import {UpdateProperty} from './components/UpdateProperty/UpdateProperty.jsx';
 import {PrivateRoute} from './components/PrivateRoute/PrivateRoute.jsx';
 import {NewProperty} from './components/NewProperty/NewProperty.jsx';
+import Navbar from './components/Navbar'
 
 debugLog(
   {
@@ -25,20 +27,24 @@ debugLog(
   }
 );
 
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
         <Router>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <PrivateRoute exact path="/update_property/:id" component={UpdateProperty} />
-            <PrivateRoute exact path='/update_properties' component={UpdateProperties} />
-            <PrivateRoute exact path='/new_property' component={NewProperty} />
-          </Switch>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/registration/" component={Registration} />
+              <PrivateRoute exact path="/update_property/:id" component={UpdateProperty} />
+              <PrivateRoute exact path='/update_properties/' component={UpdateProperties} />
+              <PrivateRoute exact path='/new_property/' component={NewProperty} />
+              <Route component={Notfound} />
+            </Switch>
+          </div>
         </Router>
-      </div>
     );
   }
 }
