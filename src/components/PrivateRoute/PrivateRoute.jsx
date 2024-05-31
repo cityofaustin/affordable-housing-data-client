ef3a2d77975e51288460e6f133f54416f737c560
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2024-05-31 10:26:27 -0500

    Update PrivateRoute.jsx

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,26 +1,10 @@
-import React, {Component} from 'react';
-import {Route, Redirect} from 'react-router-dom';
-// import {isLoggedIn} from '../Login/Login.jsx';
+import { Navigate, Outlet } from 'react-router-dom'
 
-class PrivateRoute extends Component {
-	/* constructor(props) {
-		super(props);
-	} */
-
-	render() {
-		const {component: Component, ...rest} = this.props;
-
-		if (localStorage.getItem('isLoggedIn') === 'true' && localStorage.getItem('email')) {
-			return <Route {...rest} render={(props) => <Component {...props} />}/>
-		} else {
-			return <Redirect
-				to={{
-					pathname: '/',
-					state: {from: this.props.location}
-				}}
-			/>
-		}
-	}
+const PrivateRoute = () => {
+  let auth = {'token':true}
+return (
+    auth.token ? <Outlet/> : <Navigate to='/'/>
+  )
 }
 
 export {PrivateRoute};
