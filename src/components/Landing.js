01d46791c39461b3a2690303841db5a1395efd8d
Frederick Shotlow <frederick.shotlow@austintexas.gov>
2024-05-31 10:26:11 -0500

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Landing extends Component {
     render() {
         return (
-            <div className="containter">
+            <div className="container">
                 <div className="jumbotron mt-5">
                     <div className="col-sm-8 mx-auto">
                         <h1 className="text-center">Welcome!</h1>
